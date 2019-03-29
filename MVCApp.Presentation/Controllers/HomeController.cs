7d308d1969153c48473b23a4e0f9cdaeae300264
Serfin <gracjanbrylka@gmail.com>
2019-03-29 18:48:15 +0100

    Added presentation layer, updated .gitignore (igore scripts folder)

---
+++ MVCApp.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Infrastructure.Services;
+using MVCApp.Presentation.Models;
+
+namespace MVCApp.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserService _userService = new UserService();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task Register(RegisterViewModel model)
+        {
+            await _userService.RegisterAsync(Guid.NewGuid(), model.Email, model.Ign, model.Password, "User");
+
+            RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> List()
+        {
+            var users = await _userService.GetAllAsync();
+            return View(users);
+        }
+    }
+}
---
+++ MVCApp.Presentation/Models/RegisterViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCApp.Presentation.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Insert ign")]
+        [MinLength(5, ErrorMessage = "Given ign is too short")]
+        [StringLength(100, ErrorMessage = "Given ign is too long")]
+        public string Ign { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage =  "Error")]
+        [Required(ErrorMessage = "Insert email")]
+        [MinLength(10, ErrorMessage = "Given email is too short")]
+        [StringLength(200, ErrorMessage = "Given email is too long")]
+        public string Email { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm email")]
+        [Required(ErrorMessage = "Insert email")]
+        [Compare("Email", ErrorMessage = "Emails does not match")]
+        public string ConfirmEmail { get; set; }
+
+        [DataType(DataType.Password, ErrorMessage = "Error")]
+        [Required(ErrorMessage = "Insert password")]
+        [MinLength(8, ErrorMessage = "Given password is too short")]
+        [StringLength(200, ErrorMessage = "Given password is too long")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Insert password")]
+        [Compare("Password", ErrorMessage = "Passwords does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
