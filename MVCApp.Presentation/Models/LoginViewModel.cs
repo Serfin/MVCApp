66765867ab658c3a11068ff3ff563ec0268311cf
Serfin <gracjan.brylka@gmail.com>
2019-04-13 23:49:30 +0200

    Added BrowseRotations View, cleaned up solution

--- MVCApp.Presentation/Models/LoginViewModel.cs
+++ MVCApp.Presentation/Models/LoginViewModel.cs
@@ -4,9 +4,11 @@ namespace MVCApp.Presentation.Models
 {
     public class LoginViewModel
     {
+        [Required (ErrorMessage = "Missing email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required (ErrorMessage = "Missing password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
